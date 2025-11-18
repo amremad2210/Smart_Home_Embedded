af4568ffc3de9b4a2afe6593c49d9b055c4d6790
amremad <amremad2210@gmail.com>
2025-11-18 14:37:29 +0200

    initial commit: Control & HIMI IAR Projects

---
+++ CONTROL_WS/main.c
@@ -0,0 +1,10 @@
+//hello this is control
+
+#include "tm4c123gh6pm.h"
+#include "common_macros.h"
+#include "Types.h"
+
+int main()
+{
+  return 0;
+}
---
+++ HIMI_WS/main.c
@@ -0,0 +1,11 @@
+//hello this is himi
+
+#include "tm4c123gh6pm.h"
+#include "common_macros.h"
+#include "Types.h"
+
+
+int main()
+{
+  return 0;
+}
