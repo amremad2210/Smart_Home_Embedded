b363cf51448e2fca3b0d069636f14cc4a492ec13
AndrewRRB <andrewrizk412@gmail.com>
2025-12-11 14:49:33 +0200

    refactored the buzzer

--- CONTROL_WS/inc/hal/hal_buzzer.h
+++ CONTROL_WS/inc/hal/hal_buzzer.h
@@ -1,1 +1,45 @@
-//placeholder for git
+/******************************************************************************
+ *
+ * Module: BUZZER
+ *
+ * File Name: buzzer.h
+ *
+ * Description: Header file for the Buzzer driver
+ *
+ *******************************************************************************/
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#include <stdint.h>
+#include "mcal_gpio.h" /* Included to access TivaWare definitions for Port/Pin */
+
+/*******************************************************************************
+ * Definitions                                  *
+ *******************************************************************************/
+
+/* * Configure the Buzzer Location here 
+ * NOTE: You need both the Peripheral ID (for clock) and Port Base (for IO)
+ */
+#define BUZZER_PERIPH       SYSCTL_PERIPH_GPIOF
+#define BUZZER_PORT_BASE    GPIO_PORTF_BASE
+#define BUZZER_PIN          GPIO_PIN_1
+
+/* Buzzer States */
+#define BUZZER_ON   1
+#define BUZZER_OFF  0
+
+/*******************************************************************************
+ * Function Prototypes                              *
+ *******************************************************************************/
+
+/* Initialize buzzer pin as OUTPUT */
+void BUZZER_init(void);
+
+/* Turn buzzer on or off */
+void BUZZER_setState(uint8_t state);
+
+/* Make a blocking beep (uses SysTick delay) */
+void BUZZER_beep(uint32_t duration_ms);
+
+#endif /* BUZZER_H_ */
